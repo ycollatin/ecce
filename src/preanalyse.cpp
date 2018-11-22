7dce1ee69f9084c52177238f2e78a81c13539cb2
Yves Ouvrard, alias Collatinus <yves.ouvrard@collatinus.org>
2018-11-22 21:54:26 +0100

    graphies occasionneles et systématiques

--- src/preanalyse.cpp
+++ src/preanalyse.cpp
@@ -18,64 +18,82 @@ cha>chb+
 
 #include <QFileDialog>
 #include <QTextStream>
-#include <preanalyse.h>
 
-QStringList preAn()
+#include <mainwindow.h>
+
+void MainWindow::preAn()
 {
-    QStringList liste;
     QString nf = QFileDialog::getOpenFileName(0, "Fichier à analyser", "./");
-    if (nf.isEmpty()) return liste;
+    if (nf.isEmpty()) return;
     QFile f(nf);
     if (!f.open(QFile::ReadOnly))
-        return liste;
+        return;
+    listeGr.clear();
     // chargement des 100000 premiers caractères
     QString txt;
     QTextStream flux(&f);
-    while (!flux.atEnd() && txt.length() < 10000)
+    while (!flux.atEnd())
         txt.append(flux.readLine());
     int len = txt.length();
+
     // U > V
     if (!txt.contains('U') && txt.contains('V'))
-        liste.append("U>V");
+        listeGr.append("U>V");
     // I > J
     if (!txt.contains('I') && txt.contains('J'))
-        liste.append("I>J");
+        listeGr.append("I>J");
     // ae > e
     int ae = txt.count("ae");
-    double aed = ae / len / 1.00;
-    if (aed == 0) liste.append("ae>e+");
-    else if (aed < 0.3) liste.append("ae>e");
+    if (ae == 0) listeGr.append("ae>e");
+    else
+    {
+        double aed = ae / (len + 0.0);
+        if (aed < 0.003) listeGr.append("ae;e");
+    }
+
     // h H
-    if (!txt.contains(QRegExp("\\b[Hh]")))
-        liste.append("h>-");
+    int nh = txt.count(QRegExp("\\b[Hh]"));
+    if (nh == 0) listeGr.append("h>");
+    else
+    {
+        double nhd = nh / (len+0.0);
+        if (nhd < 0.003) listeGr.append("h;");
+    }
+
     // ph f
-    if (!txt.contains("ph") && txt.contains("f"))
-        liste.append("ph>f");
+    int nph = txt.count("ae");
+    if (nph == 0) listeGr.append("ph>f");
+    else
+    {
+        double phd = nph / (len+0.0);
+        if (phd < 0.003) listeGr.append("ph;f");
+    }
+
     // ci
-    if (!txt.contains(QRegExp("ti[aeu]"))
-        && txt.contains(QRegExp("ci[aeu]")))
-        liste.append("ti>ci");
+    int nci = txt.count(QRegExp("ti[aeu]"));
+    if (nci == 0) listeGr.append("ti>ci");
+    else
+    {
+        double cid = nci / (len+0.0);
+        if (cid < 0.003) listeGr.append("ti;ci");
+            
+    }
+
     // mn/mpn
-    if (txt.contains("mpn"))
-        liste.append("mn>mpn");
+    int nmpn = txt.count("mpn");
+    if (nmpn == 0) listeGr.append("mn>mpn");
+    else
+    {
+        double mpnd = nmpn / (len+0.0);
+        if (mpnd == 0) listeGr.append("mn;mpn");
+    }
+
     // michi
-    if (txt.contains(QRegExp("\\bmichi\\b")))
-        liste.append("mihi>michi");
-    return liste;
+    int nmichi = txt.count(QRegExp("\\bmichi\\b"));
+    int nmihi = txt.count(QRegExp("\\bmihi\\b"));
+    if (nmichi > 0 && nmihi > 0) listeGr.append("mihi;michi");
+    else if (nmihi == 0) listeGr.append("mihi>michi");
+
+    qDebug()<<listeGr;
 }
 
-/*
-
-
-#  mn/mpn
-if re.search("mpn", texte):
-    print "-mpn- (dampnum)"
-else:
-    print "pas de mn > mpn"
-
-#  michi
-if re.search(r"\bmichi\b", texte):
-    print ("mihi > michi")
-else:
-    print ("mihi classique")
-*/
